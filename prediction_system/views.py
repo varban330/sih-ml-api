3c1414963766de2dc40f8f6c92cdc196fc344c12
Varun Bansal <varun330@gmail.com>
2019-02-27 03:00:54 +0530

    Prediction System 12

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -10,6 +10,6 @@ class Testview(APIView):
         return HttpResponse(json.dumps(dict))
 
     def post(self,request):
-        print(request.meta)
+        print(request.META)
         dict = {'message': 'Hello, Varun'}
         return HttpResponse(json.dumps(dict))
