eb4aa91929a12ffd92399a59855b77be73a4bd0e
Varun Bansal <varun330@gmail.com>
2019-02-27 03:15:19 +0530

    Prediction System 14

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 import json
 
+api_key = '94cea4adae3c452ebd3c2ff10dd54d7c'
 
 # Create your views here.
 class Testview(APIView):
@@ -10,6 +11,9 @@ class Testview(APIView):
         return HttpResponse(json.dumps(dict))
 
     def post(self,request):
-        print(request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"])
-        dict = {'message': 'Hello, Varun'}
-        return HttpResponse(json.dumps(dict))
+        if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
+            dict = {'message': 'Hello, Varun'}
+            return HttpResponse(json.dumps(dict), status=200)
+        else:
+            dict = {'message': 'Incorrect API Key'}
+            return HttpResponse(json.dumps(dict), status=400)
