35fa3d2133a3fedb7f994adc4176ea322f61c186
Varun Bansal <varun330@gmail.com>
2019-02-28 03:21:04 +0530

    Try exceptclauses incorporated in API

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -30,86 +30,94 @@ class TestView(APIView):
 class MessFeePred(APIView):
     def get(self, request):
         if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            dict = {'message': 'Hi,This is your developer, Varun this side'}
+            dict = {'message': 'Mess Fee Prediction'}
             return HttpResponse(json.dumps(dict),status=200)
         else:
             dict = {'message': 'Incorrect API Key'}
             return HttpResponse(json.dumps(dict), status=401)
 
     def post(self,request):
-        if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            # Preparing Data from Request
-            data = list()
-            data.append(request.data['Hostel'])
-            data.append(request.data['Mode'])
-            data.append(request.data['Bank'])
-            # Label Encoders load and transform
-            encx0 = pickle.load(open('Mess(hr)/Mess(hr)encx0.sav','rb'))
-            encx1 = pickle.load(open('Mess(hr)/Mess(hr)encx1.sav','rb'))
-            encx2 = pickle.load(open('Mess(hr)/Mess(hr)encx2.sav','rb'))
-            d = encx0.transform([data[0]])
-            data[0] = d[0]
-            d = encx1.transform([data[1]])
-            data[1] = d[0]
-            d = encx2.transform([data[2]])
-            data[2] = d[0]
-            # One hot encoder load and transform
-            onehenc = pickle.load(open('Mess(hr)/Mess(hr)onehenc.sav','rb'))
-            ar = np.array(data)
-            ar = ar.reshape(1,-1)
-            x_pred=onehenc.transform(ar).toarray()
-            k = list()
-            for i in range(20):
-                if i!=0 and i!=12 and i!=17:
-                    k.append(i)
-            x_pred = x_pred[:,k]
-            # Load Model and Predict
-            loaded_model = pickle.load(open('Mess(hr)/Mess(hr)model.sav', 'rb'))
-            result = loaded_model.predict(x_pred)
-            dict = {'days':result[0]}
-            return HttpResponse(json.dumps(dict), status=200)
-        else:
-            dict = {'message': 'Incorrect API Key'}
-            return HttpResponse(json.dumps(dict), status=401)
+        try:
+            if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
+                # Preparing Data from Request
+                data = list()
+                data.append(request.data['Hostel'])
+                data.append(request.data['Mode'])
+                data.append(request.data['Bank'])
+                # Label Encoders load and transform
+                encx0 = pickle.load(open('Mess(hr)/Mess(hr)encx0.sav','rb'))
+                encx1 = pickle.load(open('Mess(hr)/Mess(hr)encx1.sav','rb'))
+                encx2 = pickle.load(open('Mess(hr)/Mess(hr)encx2.sav','rb'))
+                d = encx0.transform([data[0]])
+                data[0] = d[0]
+                d = encx1.transform([data[1]])
+                data[1] = d[0]
+                d = encx2.transform([data[2]])
+                data[2] = d[0]
+                # One hot encoder load and transform
+                onehenc = pickle.load(open('Mess(hr)/Mess(hr)onehenc.sav','rb'))
+                ar = np.array(data)
+                ar = ar.reshape(1,-1)
+                x_pred=onehenc.transform(ar).toarray()
+                k = list()
+                for i in range(20):
+                    if i!=0 and i!=12 and i!=17:
+                        k.append(i)
+                x_pred = x_pred[:,k]
+                # Load Model and Predict
+                loaded_model = pickle.load(open('Mess(hr)/Mess(hr)model.sav', 'rb'))
+                result = loaded_model.predict(x_pred)
+                dict = {'days':result[0]}
+                return HttpResponse(json.dumps(dict), status=200)
+            else:
+                dict = {'message': 'Incorrect API Key'}
+                return HttpResponse(json.dumps(dict), status=401)
+        except Exception as e:
+            dict = {'message': e.message}
+            return HttpResponse(json.dumps(dict), status=400)
 
 
 class SchoPortPred(APIView):
     def get(self, request):
         if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            dict = {'message': 'Hi,This is your developer, Varun this side'}
+            dict = {'message': 'Scholarship Portal Prediction'}
             return HttpResponse(json.dumps(dict),status=200)
         else:
             dict = {'message': 'Incorrect API Key'}
             return HttpResponse(json.dumps(dict), status=401)
 
     def post(self,request):
-        if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            # Preparing Data from Request
-            data = list()
-            data.append(request.data['Area'])
-            data.append(request.data['Degree'])
-            # Label Encoders load and transform
-            encx0 = pickle.load(open('scholarship_portal/scholarship_portalencx0.sav','rb'))
-            encx1 = pickle.load(open('scholarship_portal/scholarship_portalencx1.sav','rb'))
-            d = encx0.transform([data[0]])
-            data[0] = d[0]
-            d = encx1.transform([data[1]])
-            data[1] = d[0]
-            # One hot encoder load and transform
-            onehenc = pickle.load(open('scholarship_portal/scholarship_portalonehenc.sav','rb'))
-            ar = np.array(data)
-            ar = ar.reshape(1,-1)
-            x_pred=onehenc.transform(ar).toarray()
-            k = list()
-            for i in range(6):
-                if i!=0 and i!=3:
-                    k.append(i)
-            x_pred = x_pred[:,k]
-            # Load Model and Predict
-            loaded_model = pickle.load(open('scholarship_portal/scholarship_portalmodel.sav', 'rb'))
-            result = loaded_model.predict(x_pred)
-            dict = {'days':result[0]}
-            return HttpResponse(json.dumps(dict), status=200)
-        else:
-            dict = {'message': 'Incorrect API Key'}
-            return HttpResponse(json.dumps(dict), status=401)
+        try:
+            if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
+                # Preparing Data from Request
+                data = list()
+                data.append(request.data['Area'])
+                data.append(request.data['Degree'])
+                # Label Encoders load and transform
+                encx0 = pickle.load(open('scholarship_portal/scholarship_portalencx0.sav','rb'))
+                encx1 = pickle.load(open('scholarship_portal/scholarship_portalencx1.sav','rb'))
+                d = encx0.transform([data[0]])
+                data[0] = d[0]
+                d = encx1.transform([data[1]])
+                data[1] = d[0]
+                # One hot encoder load and transform
+                onehenc = pickle.load(open('scholarship_portal/scholarship_portalonehenc.sav','rb'))
+                ar = np.array(data)
+                ar = ar.reshape(1,-1)
+                x_pred=onehenc.transform(ar).toarray()
+                k = list()
+                for i in range(6):
+                    if i!=0 and i!=3:
+                        k.append(i)
+                x_pred = x_pred[:,k]
+                # Load Model and Predict
+                loaded_model = pickle.load(open('scholarship_portal/scholarship_portalmodel.sav', 'rb'))
+                result = loaded_model.predict(x_pred)
+                dict = {'days':result[0]}
+                return HttpResponse(json.dumps(dict), status=200)
+            else:
+                dict = {'message': 'Incorrect API Key'}
+                return HttpResponse(json.dumps(dict), status=401)
+        except Exception as e:
+            dict = {'message': e.message}
+            return HttpResponse(json.dumps(dict), status=400)
