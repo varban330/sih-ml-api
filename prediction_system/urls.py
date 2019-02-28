4da8b5d0f750900c58caa98e465d2497de6210bb
Varun Bansal <varun330@gmail.com>
2019-02-28 22:42:49 +0530

    Url Updation

--- prediction_system/urls.py
+++ prediction_system/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^mess_fee/$', views.MessFeePred.as_view(), name="Mess_Fee"),
     url(r'^scholarship_portal/$', views.SchoPortPred.as_view(), name="Scholarship_Portal"),
     url(r'^scholarship_disbursement/$', views.SchoDisPred.as_view(), name="Scholarship_Disbursement"),
+    url(r'^water_supply/$', views.WaterSupplyPred.as_view(), name="Water_Supply"),
 ]
