091604feb15c8121ce0589930b168b4f19293582
Varun Bansal <varun330@gmail.com>
2019-03-01 14:19:17 +0530

    Electricity API incorporation

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -192,7 +192,7 @@ class SchoDisPred(APIView):
 class WaterSupplyPred(APIView):
     def get(self, request):
         if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            dict = {'message': 'Scholarship Disbursement Prediction'}
+            dict = {'message': 'Water Supply Prediction'}
             return HttpResponse(json.dumps(dict),status=200)
         else:
             dict = {'message': 'Incorrect API Key'}
@@ -253,7 +253,7 @@ class WaterSupplyPred(APIView):
 class FurniturePred(APIView):
     def get(self, request):
         if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
-            dict = {'message': 'Scholarship Disbursement Prediction'}
+            dict = {'message': 'Furniture Prediction'}
             return HttpResponse(json.dumps(dict),status=200)
         else:
             dict = {'message': 'Incorrect API Key'}
@@ -292,6 +292,63 @@ class FurniturePred(APIView):
                 x_pred = x_pred[:,k]
                 # Load Model and Predict
                 loaded_model = pickle.load(open('furniture/furnituremodel.sav', 'rb'))
+                result = loaded_model.predict(x_pred)
+                dict = {'time1':round(result[0]),
+                        'time2':max([1,round(result[0]/1.5)]),
+                        'time3':max([1,round(result[0]/2)]),
+                        'timeOF':max([1,round(result[0]/2.5)])
+                        }
+                return HttpResponse(json.dumps(dict), status=200)
+            else:
+                dict = {'message': 'Incorrect API Key'}
+                return HttpResponse(json.dumps(dict), status=401)
+        except Exception as e:
+            dict = {'message': str(e)}
+            return HttpResponse(json.dumps(dict), status=400)
+
+
+class ElectricityPred(APIView):
+    def get(self, request):
+        if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
+            dict = {'message': 'Electricty Prediction'}
+            return HttpResponse(json.dumps(dict),status=200)
+        else:
+            dict = {'message': 'Incorrect API Key'}
+            return HttpResponse(json.dumps(dict), status=401)
+
+    def post(self,request):
+        try:
+            if request.META["HTTP_OCP_APIM_SUBSCRIPTION_KEY"] == api_key:
+                # Preparing Data from Request
+                data = list()
+                data.append(request.data['Issue'])
+                data.append(request.data['Quantity'])
+                data.append(request.data['Availability'])
+                data.append(request.data['Need'])
+
+                # Label Encoders load and transform
+                encx0 = pickle.load(open('Electricity/Electricityencx0.sav','rb'))
+                encx2 = pickle.load(open('Electricity/Electricityencx2.sav','rb'))
+                encx3 = pickle.load(open('Electricity/Electricityencx3.sav','rb'))
+                d = encx0.transform([data[0]])
+                data[0] = d[0]
+                d = encx2.transform([data[2]])
+                data[2] = d[0]
+                d = encx3.transform([data[3]])
+                data[3] = d[0]
+                # One hot encoder load and transform
+                onehenc = pickle.load(open('Electricity/Electricityonehenc.sav','rb'))
+                ar = np.array(data)
+                ar = ar.reshape(1,-1)
+                x_pred=onehenc.transform(ar).toarray()
+                k = list()
+                notlist = [0,2,4]
+                for i in range(7):
+                    if i not in notlist:
+                        k.append(i)
+                x_pred = x_pred[:,k]
+                # Load Model and Predict
+                loaded_model = pickle.load(open('Electricty/Electrictymodel.sav', 'rb'))
                 result = loaded_model.predict(x_pred)
                 dict = {'time1':round(result[0]),
                         'time2':max([1,round(result[0]/1.5)]),
