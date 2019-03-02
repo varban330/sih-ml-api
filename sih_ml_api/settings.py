2ed07d526398c9d9946f704e4d87389ac58803f7
Varun Bansal <varun330@gmail.com>
2019-03-03 01:32:34 +0530

    hat

--- sih_ml_api/settings.py
+++ sih_ml_api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'sih_ml_api.urls'
