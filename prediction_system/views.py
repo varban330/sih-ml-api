a444993f5824fc827aac20141af8f6aea8340562
Varun Bansal <varun330@gmail.com>
2019-03-03 06:15:20 +0530

    Importance Roads

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -477,10 +477,13 @@ class RoadsPred(APIView):
                 # Load Model and Predict
                 loaded_model = pickle.load(open('Roads/Roadsmodel.sav', 'rb'))
                 result = loaded_model.predict(x_pred)
+                classifier = pickle.load(open('Roads/RoadsClassmodel.sav', 'rb'))
+                importance = classifier.predict(x_pred)
                 dict = {'time1':round(result[0]),
                         'time2':max([1,round(result[0]/1.5)]),
                         'time3':max([1,round(result[0]/2)]),
-                        'timeOF':max([1,round(result[0]/4)])
+                        'timeOF':max([1,round(result[0]/4)]),
+                        'importance':importance[0]
                         }
                 return HttpResponse(json.dumps(dict), status=200)
             else:
