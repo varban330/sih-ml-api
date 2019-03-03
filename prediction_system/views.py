5f5a1b9a6f8f44c548fc80453bb105f2e02ad80f
Varun Bansal <varun330@gmail.com>
2019-03-03 05:31:06 +0530

    Importance Fee

--- prediction_system/views.py
+++ prediction_system/views.py
@@ -529,10 +529,13 @@ class FeesPred(APIView):
                 # Load Model and Predict
                 loaded_model = pickle.load(open('FeePay/FeePaymodel.sav', 'rb'))
                 result = loaded_model.predict(x_pred)
+                classifier = pickle.load(open('FeePay/FeePayClassmodel.sav', 'rb'))
+                importance = classifier.predict(x_pred)
                 dict = {'time1':round(result[0]),
                         'time2':max([1,round(result[0]/1.5)]),
                         'time3':max([1,round(result[0]/2)]),
-                        'timeOF':0
+                        'timeOF':0,
+                        'importance':importance[0]
                         }
                 return HttpResponse(json.dumps(dict), status=200)
             else:
